b6d42217f051a0d80ad0a4ad81b692ad78276a76
marinados <marina.starkova@gmail.com>
2015-12-11 15:58:44 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,12 @@ Device.destroy_all
   Category.create(label: type)
 end
 
-[
-  'Quentin Bordage': 'quentin',
-  'Julien Boulenguer': 'julien',
-  'Blandine Freté': 'blandine',
-  'François Blanchard': 'francois',
-  'Victor Thépaut': 'victor',
-  'Marie Jocelyn': 'mariej',
-  'Lucie Thiollière': 'lucie',
-  'Lucas Khidichian': 'lucas',
-  'Ludivine Chotard': 'ludivine',
-  'Walid Seoudi': 'walid',
-  'Tristan Latrasse': 'tristan',
-  'Jean-Baptiste Beck': 'jean-baptiste',
-  'Mathilde Odin': 'mathilde',
-  'Alix Dumarest': 'alix',
-  'Benoit Miegeville': 'benoit',
-  'Kenneth Grand': 'kenneth',
-  'Eléonore Péron': 'eleonore',
-  'Thibault Egron': 'thibault',
-  'Marina Starkova': 'marina',
-  'Cwiosna Roques': 'cwiosna',
-  'Yasmine Benhammada': 'yasmine'
-].each do |person|
-  person.each { |k, v| User.create(name: k, email: v + '@brandandcelebrities.com') }
-end
+[{'Quentin Bordage': 'quentin'}, {'Julien Boulenguer': 'julien'}, {'Blandine Freté': 'blandine'},
+  {'François Blanchard': 'francois'}, {'Victor Thépaut': 'victor'}, {'Marie Jocelyn': 'mariej'},
+  {'Lucie Thiollière': 'lucie'}, {'Lucas Khidichian': 'lucas'}, {'Ludivine Chotard': 'ludivine'},
+  {'Walid Seoudi': 'walid'}, {'Tristan Latrasse': 'tristan'}, {'Jean-Baptiste Beck': 'jean-baptiste'},
+  {'Mathilde Odin': 'mathilde'}, {'Alix Dumarest': 'alix'}, {'Benoit Miegeville': 'benoit'},
+  {'Kenneth Grand': 'kenneth'}, {'Eléonore Péron': 'eleonore'}, {'Thibault Egron': 'thibault'},
+  {'Marina Starkova': 'marina'}, {'Cwiosna Roques': 'cwiosna'}, {'Yasmine Benhammada': 'yasmine'}].each do |person|
+    person.each { |k, v| User.create(name: k, email: v + '@brandandcelebrities.com') }
+  end
